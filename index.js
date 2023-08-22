f65a9a08aaadc90b7174b737d79121e12b9f5771
xbarrigac <xbarrigac@gmail.com>
2023-08-22 17:15:22 -0500

    Path * agregado

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ app.use('/api/auth', require('./routes/auth'));
 // TODO: CRUD: Eventos
 app.use('/api/events', require('./routes/events'));
 
+app.get('*', (req, res) => {
+    res.sendFile( __dirname + '/public/index.html');
+})
+
 // Escuchar peticiones
 app.listen( process.env.PORT, () => {    
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
